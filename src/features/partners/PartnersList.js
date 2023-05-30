2b1c5aa28171d40cb551f3e8332978ba30779798
alexamankwah2 <amankwahalex22@gmail.com>
2023-05-30 03:30:36 -0400

    seccond commit

---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import { Col, Row} from 'reactstrap';
+// import { PARTNERS } from '../../app/shared/PARTNERS';
+import Partner from './Partner';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
+import { selectAllPartners } from './partnersSlice';
+
+const PartnerList = () => {
+    const partners= useSelector(selectAllPartners);
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
+
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
+        <Col className='mt-4'>
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
+        </Col>
+    );
+};
+export default PartnerList;
